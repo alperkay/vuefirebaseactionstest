3ec7e7afea4b720c26393cd21bdf75188275076f
Alper <alper@Alpers-MBP.local>
2020-10-13 22:58:36 +0200

    painting test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,60 @@
 <template>
   <div class="home">
-    <h1>is this deployin? it shouldn't!</h1>
-    <h2>just checkin</h2>
-    <h3>well</h3>
+    <canvas
+      @mousedown="startPosition"
+      @mouseup="finishedPosition"
+      @mousemove="draw"
+      id="canvas"
+    ></canvas>
   </div>
 </template>
 
 <script lang="ts">
+import { CanvasHTMLAttributes } from "vue";
 import { Options, Vue } from "vue-class-component";
 
-@Options({
-  components: {},
-})
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private painting = false;
+  private canvas: any;
+  private ctx: any;
+
+  public mounted() {
+    this.canvas = document.querySelector("#canvas");
+    this.ctx = this.canvas.getContext("2d");
+
+    this.canvas.height = window.innerHeight;
+    this.canvas.width = window.innerWidth;
+
+    this.canvas.addEventListener("touchstart", this.startPosition);
+    this.canvas.addEventListener("touchend", this.finishedPosition);
+    this.canvas.addEventListener("touchmove", this.draw);
+  }
+
+  private startPosition(e: MouseEvent) {
+    this.painting = true;
+    this.draw(e);
+  }
+
+  private finishedPosition() {
+    this.painting = false;
+    this.ctx.beginPath();
+  }
+
+  private draw(e: MouseEvent) {
+    if (!this.painting) return;
+    this.ctx.lineWidth = 10;
+    this.ctx.lineCap = "round";
+
+    this.ctx.lineTo(e.clientX, e.clientY);
+    this.ctx.stroke();
+    this.ctx.beginPath();
+    this.ctx.moveTo(e.clientX, e.clientY);
+  }
+}
 </script>
+
+<style lang="scss">
+#canvas {
+  border: 2px solid black;
+}
+</style>
