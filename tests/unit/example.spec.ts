21fd77894d71f32f70d12f40b6422badee9a0c35
Alper <alper@Alpers-MBP.local>
2020-10-13 08:17:11 +0200

    test unit tests

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,9 +1,9 @@
 import { shallowMount } from "@vue/test-utils";
-import Home from "@/components/Home.vue";
+import Home from "@/views/Home.vue";
 
 describe("Home.vue", () => {
   it("renders props.msg when passed", () => {
     const wrapper = shallowMount(Home);
-    expect(wrapper.text()).toMatch("hey there!");
+    expect(wrapper.text()).toMatch("not hey there!");
   });
 });
