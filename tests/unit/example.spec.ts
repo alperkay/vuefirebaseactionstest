06e880761e3b8fb9cf2183ce9ee34728ee2e0bdd
Alper <alper@Alpers-MBP.local>
2020-10-13 08:59:11 +0200

    passing test should trigger build

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -4,6 +4,6 @@ import Home from "@/views/Home.vue";
 describe("Home.vue", () => {
   it("renders props.msg when passed", () => {
     const wrapper = shallowMount(Home);
-    expect(wrapper.text()).toMatch("not hey there!");
+    expect(wrapper.text()).toMatch("is this deployin? it shouldn't!");
   });
 });
